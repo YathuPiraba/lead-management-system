9a5b1e88fbc708fc6c30ae170f0d7302c2b9ab68
YathuPiraba <yathavan1407@gmail.com>
2024-11-25 10:28:18 +0530

    added packages

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Get the API prefix from the configuration
+  const configService = app.get(ConfigService);
+  const apiPrefix = configService.get<string>('API_PREFIX', 'api/v1');
+  app.setGlobalPrefix(apiPrefix);
+
   await app.listen(3000);
 }
 bootstrap();
