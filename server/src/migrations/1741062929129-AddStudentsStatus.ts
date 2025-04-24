82784ede3ab4093ab3f6c3f131c69ceddbb7fbe6
YathuPiraba <yathavan1407@gmail.com>
2025-04-24 17:32:02 +0530

    added docker

--- server/src/migrations/1741062929129-AddStudentsStatus.ts
+++ server/src/migrations/1741062929129-AddStudentsStatus.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class AddStudentStatus1700000000000 implements MigrationInterface {
+export class AddStudentStatus1741062929129 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `ALTER TABLE "students" ADD COLUMN "status" character varying(50) NOT NULL DEFAULT 'lead' CHECK ("status" IN ('hold', 'active', 'lead','reject'))`,
