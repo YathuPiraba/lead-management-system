9946e4cfb2960717164b286a3809684f9d002f47
Thavakethan <kethanthava12@email.com>
2025-07-07 12:34:48 +0530

    fix: add ConfigModule import to MailModule for configuration support

--- server/src/mail/mail.module.ts
+++ server/src/mail/mail.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule],
   providers: [MailService],
   exports: [MailService],
 })
